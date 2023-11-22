215a01c4df29f4590933773aebe6a07978d04fcf
loshak <loshak@loshak.loshak.ivan.org>
2023-11-22 15:04:46 +0700

    first commfft

--- BH_Loshak.sh
+++ BH_Loshak.sh
@@ -2,23 +2,45 @@
 echo "Program for work with checksum MD5" 
 echo "Developer: Loshak Ivan" 
 pwd
-readlink -f $(ls | sed -n '1p')
-md5sum $(ls | sed -n '1p')
-readlink -f $(ls | sed -n '2p')
-if [ $(stat -c%s $(ls | sed -n '2p')) -eq 0 ];
-then printf $(md5sum $(ls | sed -n '1p')) > $(readlink -f $(ls | sed -n '2p'))
-echo "Data in file " $(ls | sed -n '2p') ": " $(cat $(readlink -f $(ls | sed -n '2p')))
+while true
+do
+echo "Enter first file:"
+read f1
+if !([ -f $f1 ]);
+then echo "This file not exit"
+break
+fi
+md5sum $f1
+echo "Enter second file:"
+read f2
+if !([ -f $f2 ]);
+then echo "This file not exit"
+break
+fi
+if [ $(stat -c%s $f1) -eq 0 ];
+then printf $(md5sum $f1) > $f2
+echo "Data in file " $f2 ": " $(cat $f2)
 else 
-echo "Data in file " $(ls | sed -n '2p') ": " $(cat $(readlink -f $(ls | sed -n '2p'))) 
+echo "Data in file " $f2 ": " $(cat $f2) 
 echo "You want rewrite file [Y/N]? Daefault No:"
 read Ans;
 if [ $Ans = "y" ];
-then printf $(md5sum $(ls | sed -n '1p')) > $(readlink -f $(ls | sed -n '2p'))
-echo "Data in file " $(ls | sed -n '2p') ": " $(cat $(readlink -f $(ls | sed -n '2p')))
+then printf $(md5sum $f1) > $f2
+echo "Data in file " $f2 ": " $(cat $f2)
 elif [ $Ans = "Y" ];
-then printf $(md5sum $(ls | sed -n '1p')) > $(readlink -f $(ls | sed -n '2p'))
-echo "Data in file " $(ls | sed -n '2p') ": " $(cat $(readlink -f $(ls | sed -n '2p')))
+then printf $(md5sum $f1) > $f2
+echo "Data in file " $f2 ": " $(cat $f2)
 else echo "You didn't rewrite file";
 fi
 fi
+echo "You want generate checksum again? [Y/N]. Default N";
+read AnsW
+if [ AnsW = "y"];
+then echo "You generate new checksum";
+elif [ AnsW = "y"];
+then echo "You generate new checksum";
+else echo "Stop pragramm"
+break
+fi
+done
 
